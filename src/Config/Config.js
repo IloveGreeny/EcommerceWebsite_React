4e11280e1d025b3ae23a54b8529a1866bfef1115
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-06-27 14:20:21 +0400

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -5,14 +5,6 @@ import 'firebase/firestore';
 import 'firebase/auth'
 
 const firebaseConfig = { /* Your Firebase Config
-    apiKey: "AIzaSyDGmVBx5LAYJuIq_Ni1nZBenfcVUFX-SyY",
-    authDomain: "ecommerce-with-react-2ac06.firebaseapp.com",
-    databaseURL: "https://ecommerce-with-react-2ac06.firebaseio.com",
-    projectId: "ecommerce-with-react-2ac06",
-    storageBucket: "ecommerce-with-react-2ac06.appspot.com",
-    messagingSenderId: "690603499200",
-    appId: "1:690603499200:web:09860bc318b5b7fd74d725",
-    measurementId: "G-88N35MC51Q"
 */};
     
 firebase.initializeApp(firebaseConfig);
